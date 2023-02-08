40798470652d1c620f8262bdac621346a5e37095
reinanmat <reinan266@gmail.com>
2023-02-08 17:49:39 -0300

    finish 04 and so close 05

---
+++ MiniPiscina-02/ex05/elements.py
@@ -0,0 +1,89 @@
+from elem import *
+
+class Html(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="html", content=content)
+
+class Head(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="head", content=content)
+
+class Body(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="body", content=content)
+
+class Title(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="title", content=Text(content))
+
+class Meta(Elem):
+    def __init__(self, attr={}):
+        super().__init__(tag="meta", attr=attr, tag_type="simple")
+
+class Img(Elem):
+    def __init__(self, attr={}):
+        super().__init__(tag="img", attr=attr, tag_type="simple")
+
+class Table(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="table", content=content)
+
+class Th(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="th", content=content)
+
+class Tr(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="tr", content=content)
+
+class Td(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="td", content=content)
+
+class Ul(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="ul", content=content)
+
+class Ol(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="ol", content=content)
+
+class Li(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="li", content=content)
+
+class H1(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="h1", content=content)
+
+class H2(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="h2", content=content)
+
+class P(Elem):
+    def __init__(self, content=""):
+        super().__init__(tag="p", content=content)
+
+class Div(Elem):
+    def __inti__(self, content=""):
+        super().__init__(content=content)
+
+class Span(Elem):
+    def __init__(self, attr={}):
+        super().__init__(tag="span", attr=attr)
+
+class Hr(Elem):
+    def __init__(self, attr={}):
+        super().__init__(tag="hr", attr=attr)
+
+
+class Br(Elem):
+    def __init__(self, attr={}):
+        super().__init__(tag="br", attr=attr)
+
+
+def main():
+    print(Html(Head(Title())))
+
+if __name__ == '__main__':
+    main()
