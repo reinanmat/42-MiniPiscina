cc174ea2ae5e9d34ac463ef7742380fcba5475d0
reinanmat <reinan266@gmail.com>
2023-02-09 17:34:02 -0300

    init 03

--- MiniPiscina-03/ex00/geohashing.py
+++ MiniPiscina-03/ex00/geohashing.py
@@ -1,12 +1,16 @@
 import sys
+import antigravity
+
+def check_args():
+    if (len(sys.argv) != 4):
+        sys.exit("Error: wrong number of arguments")
 
 def main():
-    hash1 = hash(sys.argv[1])
-    hash2 = hash(sys.argv[2])
-    hash3 = hash(sys.argv[3])
-    print(hash1)
-    print(hash2)
-    print(hash3)
+    check_args()
+    latitude = float(sys.argv[1])
+    longitude = float(sys.argv[2])
+    date = sys.argv[3].encode('utf-8')
+    antigravity.geohash(latitude, longitude, date)
 
 if __name__ == '__main__':
     main()
